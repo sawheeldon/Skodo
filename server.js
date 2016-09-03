838528126632557f0784a4506a344809fb791e93
Simon <simon@jampot.design>
2016-09-03 18:19:28 +0000

    test basics inplace

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 var express = require ('express');
-
 var app = express();
+var expect  = require("chai").expect;
+var request = require("request");
+
 app.use(express.static('public'));
 app.listen(process.env.PORT || 8080);
