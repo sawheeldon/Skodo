724979259957bd58aa93fb5efa234c76910ab1a6
Simon <simon@jampot.design>
2016-09-12 00:50:48 +0000

    jquery working in future dont add API calls in server js, it needs to b e in main js

--- public/main.js
+++ public/main.js
@@ -1,16 +1,35 @@
 /* global $ */
 
 
-$(function(data) {
-        $.ajax({
-            type: 'GET',
-            url:'https://api.soccerama.pro/v1.1/competitions?api_token=3VIR2AoOrayqSFUUhMuVJBWclkzbtXrH712ilcavqLqIboT6qo3Gatc8QYk0&include=country,currentSeason',
-            success: function(data){
-                console.log('success', data);
-            }
-            });
-});
+var latestNews = function (data) {
+  $.ajax({
+      type: 'GET',
+      url:'https://newsapi.org/v1/articles?source=bbc-sport&sortBy=top&apiKey=9e6d16842a6b4368b4937a31ccf54035',
+      success: function(data){
+          console.log('success', data);
+          console.log(data);
 
+          // var denCurrent = data.data[0].currentSeason.name;
+
+          // document.getElementById("currentLeagues").innerHTML = denCurrent ;
+      }
+  })
+};
+
+// $(function(data) {
+//         $.ajax({
+//             type: 'GET',
+//             url:'https://api.soccerama.pro/v1.1/competitions?api_token=3VIR2AoOrayqSFUUhMuVJBWclkzbtXrH712ilcavqLqIboT6qo3Gatc8QYk0&include=country,currentSeason',
+//             success: function(data){
+//                 console.log('success', data);
+//                 console.log(data.data[0].currentSeason);
+
+//                 var denFlag = data.data[0].country;
+
+//                 document.getElementById("currentLeagues").innerHTML = denFlag ;
+//             }
+//             });
+// });
 // function displayLeagues(event) {
 //         var buildTheHtmlOutput = "";
 
@@ -29,6 +48,6 @@ $(function(data) {
 
 $(document).ready(function() {
     
-    
+    latestNews();
     
 });
